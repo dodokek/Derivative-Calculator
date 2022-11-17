2220259f2b682f5cb082d9d8d663ab9bd41a48b2
Aleksander Morozov <alex.morozov.dd@mail.ru>
2022-11-17 14:57:30 +0300

    Copied some from akinator

---
+++ include/derivative.h
@@ -0,0 +1,126 @@
+#ifndef derivative_H
+#define derivative
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <Mmsystem.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "fileUtils.h"
+#include "stack.h"
+#include "stringUtils.h"
+
+
+//-----------------------------------------------
+
+enum Positions
+{
+    LEFT,
+    RIGHT,
+};
+
+enum Answers
+{
+    NO,
+    YES,
+};
+
+
+enum Sizes
+{
+    MAX_IMG_SRC_LEN = 100,
+    STACK_INIT_CAP = 5,
+    MAX_NAME_LEN = 100,
+    MAX_MESSAGE_LEN =1000,
+};
+
+
+enum PlayMode
+{
+    EXIT = 0,
+    GUESS = 1,
+    LISTING = 2,
+    COMPARISON = 3,
+};
+
+const int OFFSET = 2;
+
+//-----------------------------------------------
+
+
+struct node
+{
+    node* parent;
+
+    node* left;
+    node* right;
+    // 
+
+    char* name;
+};
+
+
+#define $YES                                                                        \
+    mciSendString("open \"data/yes.mp3\" type mpegvideo alias mp3", NULL, 0, NULL); \
+    mciSendString("play mp3", NULL, 0, NULL);                                       
+
+
+void StartGame (node* root);
+
+int GetAnswer ();
+
+bool isNegativeAns (node* cur_node);
+
+node* GetNodeFromUser (node* root);
+
+node* GetTreeRoot ();
+
+void SaveProgress (node* root);
+
+node* InsertNode (char name[], node* parent, Positions position = LEFT);
+
+node* CreateNewNode ();
+
+node* InitTreeRoot (char name[]);
+
+void GuessNode (node* cur_node);
+
+char* GetInput (char* buffer);
+
+void AddNode (node* cur_node);
+
+void DumpTree (node* root);
+
+void DrawTree (node* root);
+
+void SayWords (char* templ, ...);
+
+void PrintPreOrder (node* node, FILE* tree_data);
+
+void PrintPostOrder (node* node, FILE* tree_data);
+
+void PrintObject (node* node_to_print);
+
+Stack BuildAncestorsStack (node* cur_node);
+
+void AddAncestor (node* cur_node, Stack* ancestors);
+
+void CompareObjects (node* obj1, node* obj2);
+
+void InitGraphvisNode (node* node, FILE* dot_file);
+
+void RecursDrawConnections (node* node, FILE* dot_file);
+
+node* FindNode (node* cur_node, const char name[]);
+
+node* BuildTree (FILE* tree_info);
+
+node* RecBuildNode (Text* input, int* obj_counter);
+
+node* DestructTree (node* root);
+
+
+
+#endif
---
+++ src/derivative.cpp
@@ -0,0 +1,381 @@
+
+#include "derivative.h"
+
+int main()
+{
+    node* root = GetTreeRoot();
+
+    StartGame (root);
+
+    SaveProgress (root);
+
+    DestructTree (root);
+}
+
+
+node* GetTreeRoot ()
+{
+    FILE* tree_data = get_file ("data/tree.txt", "r");
+    
+    node* root = BuildTree (tree_data);
+
+    DumpTree (root);
+    
+    fclose (tree_data);
+
+    return root;
+}
+
+
+node* InitTreeRoot (char name[])
+{
+    node* root = CreateNewNode();
+
+    root->name = name;
+
+    return root;
+}
+
+
+node* CreateNewNode ()
+{
+    node* new_node = (node*) calloc (1, sizeof (node));
+    if (!new_node) return nullptr;
+
+    new_node->left   = nullptr;
+    new_node->right  = nullptr;
+    new_node->parent = nullptr;
+
+    new_node->name = (char*) calloc (MAX_NAME_LEN, sizeof (char)); 
+
+    return new_node;
+}
+
+
+node* DestructTree (node* root)
+{
+    if (root->left)  DestructTree (root->left);
+    if (root->right) DestructTree (root->right);
+
+    root->name = nullptr;
+    
+    free (root);
+    root = nullptr;
+
+    return root;
+}
+
+
+// Positions 
+node* InsertNode (char name[], node* parent, Positions position)
+{
+    node* new_node = CreateNewNode();
+    
+    new_node->name = name;
+    new_node->parent = parent;
+
+    if (position == LEFT)
+    {
+        if (parent->left == nullptr) parent->left = new_node;
+        else
+        {
+            new_node->left = parent->left;
+            parent->left->parent = new_node;
+            parent->left = new_node;
+        }
+    }
+    else if (position == RIGHT)
+    {
+        if (parent->right == nullptr) parent->right = new_node;
+        else
+        {
+            new_node->left = parent->right;
+            parent->right->parent = new_node;
+            parent->right = new_node;
+        }
+    }
+
+    return new_node;
+}
+
+
+node* FindNode (node* cur_node, const char name[])
+{
+    if (!cur_node) return nullptr;
+
+    if (strcmp (cur_node->name, name) == 0) return cur_node;
+
+    node* find_left = FindNode (cur_node->left, name);
+    node* find_right = FindNode (cur_node->right, name);
+
+    if (find_left) return find_left;
+    if (find_right) return find_right;
+
+    return nullptr;
+}
+
+
+node* BuildTree (FILE* tree_info)
+{
+    Text input = {};
+    GetTreeObjects (&input, tree_info);
+    int obj_counter = 1;
+
+    for (int i = 0; i < input.lines_amount; i++)
+    {
+        printf ("Got line %s \n ", input.objects[i].begin);
+    }
+
+    node* root = CreateNewNode();
+    root->name = input.objects[obj_counter].begin;
+    printf ("Root: %s \n ", root->name);
+    obj_counter++;
+
+    if (*input.objects[obj_counter].begin == '}') return root;
+
+    obj_counter++;
+    root->left  = RecBuildNode (&input, &obj_counter);
+    root->left->parent = root;
+
+    root->right = RecBuildNode (&input, &obj_counter);
+    root->right->parent = root;
+
+    return root;
+}
+
+
+node* RecBuildNode (Text* input, int* obj_counter)
+{
+    while (*input->objects[*obj_counter].begin == '{' || *input->objects[*obj_counter].begin == '}')
+    {
+        (*obj_counter)++; // skips all unneeded brackets
+    }
+    
+    node* new_node = CreateNewNode();
+    new_node->name = input->objects[*obj_counter].begin;
+
+    printf ("Got node %s \n ", new_node->name);
+
+    (*obj_counter)++;
+
+    if (*input->objects[*obj_counter].begin == '}')
+    {
+        (*obj_counter)++; // skips '}'
+        return new_node;
+    }
+
+    new_node->left = RecBuildNode (input, obj_counter);
+    new_node->left->parent = new_node;
+
+    new_node->right = RecBuildNode (input, obj_counter);
+    new_node->right->parent = new_node;
+
+    return new_node;
+}
+
+//------------------------Tree builder--------------------
+
+
+char* GetInput (char* buffer)
+{
+    char* check_input = fgets(buffer, MAX_NAME_LEN, stdin);
+
+    if (check_input == NULL) 
+    {
+        printf ("Bad input");
+        return NULL;
+    }
+
+    buffer[strlen(buffer) - 1] = '\0';
+
+    while (strlen(buffer) == 0) 
+    {
+        printf("\n Invalid name, write it again. \n\n");
+
+        check_input = fgets(buffer, MAX_NAME_LEN, stdin);
+
+        if (check_input == NULL) return NULL;
+
+        buffer[strlen(buffer)] = '\0';
+    }
+
+    return buffer;
+}
+
+//------------------------Guessing mode. End-----------------------
+
+
+//------------------------Object find mode----------------
+
+void PrintObject (node* node_to_print)
+{
+    printf ("Characteristics of object %s:", node_to_print->name);
+    
+    Stack ancestors = BuildAncestorsStack (node_to_print);    
+
+    node* tmp_node = nullptr;
+
+    while (ancestors.size != 0)
+    {
+        tmp_node = (node*) StackPop (&ancestors);
+        if (isNegativeAns (tmp_node)) SayWords ("NOT %s - ", tmp_node->name);
+        else   
+        {                    
+            printf ("%s - ", tmp_node->name);
+        }
+    }
+
+    // tmp_node = (node*) StackPop (&ancestors);
+    // SayWords ("%s.\n", tmp_node->name);
+}
+
+
+bool isNegativeAns (node* cur_node)
+{
+    if (!(cur_node->parent)) return false;
+
+    // printf ("I am %s, %p My parent is %p he calls %p\n",
+    //         cur_node->name, cur_node, cur_node->parent, cur_node->parent->left);
+
+    if (cur_node->parent->right == cur_node) return true;
+
+    return false;
+}
+
+
+Stack BuildAncestorsStack (node* cur_node)
+{
+    Stack ancestors = {0};
+    StackCtor (&ancestors, 10);
+    StackPush (&ancestors, cur_node);
+    
+    AddAncestor (cur_node->parent, &ancestors);
+    
+    return ancestors;
+}
+
+
+void AddAncestor (node* cur_node, Stack* ancestors)
+{
+    StackPush (ancestors, cur_node);
+
+    if (cur_node->parent) AddAncestor (cur_node->parent, ancestors);
+}
+
+//------------------------Dump----------------------------
+
+void SaveProgress (node* root)
+{
+    FILE* tree_data = get_file ("data/tree.txt", "w+");
+
+    PrintPreOrder (root, tree_data);
+    fclose (tree_data);
+    DrawTree (root);
+}
+
+
+void DumpTree (node* node)
+{
+    assert (node);
+
+    printf ("Ptr[%p] : \n", node);
+    printf ("\t Node %s: left %p, right %p, parent %p, %s\n",
+            node->name, node->left,
+            node->right, node->parent, node->name);
+
+    if (node->left)  DumpTree (node->left);
+    if (node->right) DumpTree (node->right);
+}
+
+
+void PrintPreOrder (node* node, FILE* tree_data)
+{
+    fprintf (tree_data, "{\n%s\n", node->name);
+    if (node->left)  PrintPreOrder (node->left,  tree_data);
+    if (node->right) PrintPreOrder (node->right, tree_data);
+    fprintf (tree_data, "}\n");
+}
+
+
+void PrintPostOrder (node* node, FILE* tree_data)
+{
+    if (node->left)  PrintPreOrder (node->left,  tree_data);
+    if (node->right) PrintPreOrder (node->right, tree_data);
+    fprintf (tree_data, "{\n%s\n", node->name);
+    fprintf (tree_data, "}\n");
+} 
+
+
+#define _print(...) fprintf (dot_file, __VA_ARGS__)
+
+void DrawTree (node* root)
+{
+    FILE* dot_file = get_file ("data/graph.dot", "w+");
+    
+    // Writing header info
+    const char header[] = R"(
+    digraph g {
+        dpi      = 200;
+        fontname = "Comic Sans MS";
+        fontsize = 20;
+        rankdir   =  TB;
+        edge [color = darkgrey, arrowhead = onormal, arrowsize = 1, penwidth = 1.2]
+        graph[fillcolor = lightgreen, ranksep = 1.3, nodesep = 0.5,
+        style = "rounded, filled",color = green, penwidth = 2]
+
+    )";
+    
+    _print (header);
+
+    InitGraphvisNode (root, dot_file);
+
+    RecursDrawConnections (root, dot_file);
+
+    _print ("}\n");
+    
+    // Executing dotfile and printing an image
+
+    fclose (dot_file);
+
+    system ("dot -Tpng data/graph.dot -o data/pretty_tree.png");
+
+    return;
+}
+
+
+void InitGraphvisNode (node* node, FILE* dot_file)   // Recursivly initialises every node 
+{
+    _print ("Node%p[shape=rectangle, color=\"red\", width=0.2, style=\"filled\","
+            "fillcolor=\"lightblue\", label=\"%s\"] \n \n",
+            node, node->name);
+    
+    if (node->left) InitGraphvisNode (node->left, dot_file);
+    if (node->right) InitGraphvisNode (node->right, dot_file);
+
+    return;
+}
+
+
+void RecursDrawConnections (node* node, FILE* dot_file)
+{
+    if (node->left)
+    {
+        _print ("Node%p->Node%p\n", node, node->left);
+        RecursDrawConnections (node->left, dot_file);
+    } 
+    if (node->right)
+    {
+        _print ("Node%p->Node%p\n", node, node->right);
+        RecursDrawConnections (node->right, dot_file);
+    } 
+
+    return;
+}
+
+
+#undef _print
+
+//------------------------Dump----------------------------
+
+
+
